167f67e83e8d609dd6f380f6d1569d7b600981a7
davaz <davazamb10@outlook.com>
2020-04-08 16:56:06 -0400

    Add DataContext CodeFirst EF, Initial Migrations

---
+++ SoccerForecast.Web/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoccerForecast.Web.Data.Entities;
+
+namespace SoccerForecast.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TeamEntity> Teams { get; set; }
+    }
+
+}
--- SoccerForecast.Web/Startup.cs
+++ SoccerForecast.Web/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SoccerForecast.Web.Data;
 
 namespace SoccerForecast.Web
 {
@@ -30,6 +32,12 @@ namespace SoccerForecast.Web
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+
+            services.AddDbContext<DataContext>(cfg =>
+            {
+                cfg.UseSqlServer(Configuration.GetConnectionString("SoccerForescastConnection"));
+            });
+
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
